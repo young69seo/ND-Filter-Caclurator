ab495b06981b6b814f1410aa418b5cacf20d7345
Young Seo <young69.seo@gmail.com>
2026-02-15 10:46:34 +1100

    Initial Commit

---
+++ ND Filter Caclurator/ND_Filter_CacluratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ND_Filter_CacluratorApp.swift
+//  ND Filter Caclurator
+//
+//  Created by Young Joo Seo on 15/2/2026.
+//
+
+import SwiftUI
+
+@main
+struct ND_Filter_CacluratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
